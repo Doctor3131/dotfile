53292b61966de93f963d50939d025c479e399bf0
Doctor3131 <sirielwnf@gmail.com>
2025-09-19 08:36:50 +0700

    error

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,67 +1,140 @@
+-- return {
+-- 	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+--
+-- 	{
+-- 		"nvim-treesitter/nvim-treesitter",
+-- 		opts = {
+-- 			ensure_installed = {
+-- 				"astro",
+-- 				"cmake",
+-- 				"cpp",
+-- 				"css",
+-- 				"fish",
+-- 				"gitignore",
+-- 				"go",
+-- 				"graphql",
+-- 				"http",
+-- 				"java",
+-- 				"php",
+-- 				"rust",
+-- 				"scss",
+-- 				"sql",
+-- 				"svelte",
+-- 			},
+--
+-- 			-- matchup = {
+-- 			-- 	enable = true,
+-- 			-- },
+--
+-- 			-- https://github.com/nvim-treesitter/playground#query-linter
+-- 			query_linter = {
+-- 				enable = true,
+-- 				use_virtual_text = true,
+-- 				lint_events = { "BufWrite", "CursorHold" },
+-- 			},
+--
+-- 			playground = {
+-- 				enable = true,
+-- 				disable = {},
+-- 				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+-- 				persist_queries = true, -- Whether the query persists across vim sessions
+-- 				keybindings = {
+-- 					toggle_query_editor = "o",
+-- 					toggle_hl_groups = "i",
+-- 					toggle_injected_languages = "t",
+-- 					toggle_anonymous_nodes = "a",
+-- 					toggle_language_display = "I",
+-- 					focus_language = "f",
+-- 					unfocus_language = "F",
+-- 					update = "R",
+-- 					goto_node = "<cr>",
+-- 					show_help = "?",
+-- 				},
+-- 			},
+-- 		},
+-- 		config = function(_, opts)
+-- 			require("nvim-treesitter.configs").setup(opts)
+--
+-- 			-- MDX
+-- 			vim.filetype.add({
+-- 				extension = {
+-- 					mdx = "mdx",
+-- 				},
+-- 			})
+-- 			vim.treesitter.language.register("markdown", "mdx")
+-- 		end,
+-- 	},
+-- }
 return {
-	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+  -- Ensure the nvim-treesitter plugin and its related extensions are loaded together.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/playground" },
+    build = function()
+      -- This command ensures that all the parsers you specified are installed.
+      vim.cmd("TSUpdateSync")
+    end,
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+      },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"astro",
-				"cmake",
-				"cpp",
-				"css",
-				"fish",
-				"gitignore",
-				"go",
-				"graphql",
-				"http",
-				"java",
-				"php",
-				"rust",
-				"scss",
-				"sql",
-				"svelte",
-			},
+      -- matchup = {
+      -- 	enable = true,
+      -- },
 
-			-- matchup = {
-			-- 	enable = true,
-			-- },
+      -- https://github.com/nvim-treesitter/playground#query-linter
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
 
-			-- https://github.com/nvim-treesitter/playground#query-linter
-			query_linter = {
-				enable = true,
-				use_virtual_text = true,
-				lint_events = { "BufWrite", "CursorHold" },
-			},
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25,
+        persist_queries = true,
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+    },
+    config = function(_, opts)
+      -- The configuration logic is now run after the plugin and its dependencies are guaranteed to be loaded.
+      require("nvim-treesitter.configs").setup(opts)
 
-			playground = {
-				enable = true,
-				disable = {},
-				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-				persist_queries = true, -- Whether the query persists across vim sessions
-				keybindings = {
-					toggle_query_editor = "o",
-					toggle_hl_groups = "i",
-					toggle_injected_languages = "t",
-					toggle_anonymous_nodes = "a",
-					toggle_language_display = "I",
-					focus_language = "f",
-					unfocus_language = "F",
-					update = "R",
-					goto_node = "<cr>",
-					show_help = "?",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-
-			-- MDX
-			vim.filetype.add({
-				extension = {
-					mdx = "mdx",
-				},
-			})
-			vim.treesitter.language.register("markdown", "mdx")
-		end,
-	},
+      -- MDX filetype configuration
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
 }
